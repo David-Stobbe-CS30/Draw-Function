fefc253c28404febadbace1db547d05574990150
David Stobbe <d.stobbe1@share.epsb.ca>
2023-09-08 12:27:37 -0600

    started

--- main.py
+++ main.py
@@ -2,5 +2,16 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 
+def drawCircle(color, rad, x, y):
+    pygame.draw.circle(screen, color, pygame.Vector2(x, y), rad)
+
+running = True
 while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill("white")
+    
+    drawCircle("red", 40, 30, 30)
     
